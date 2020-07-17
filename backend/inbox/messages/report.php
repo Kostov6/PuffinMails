b645d0ee62864e9c9496374c2dc77f9229f78256
Kostov6 <angliyskiwiktor@gmail.com>
2020-07-17 15:34:51 +0300

    Until 17.7 new

--- backend/inbox/messages/report.php
+++ backend/inbox/messages/report.php
@@ -1,17 +1,22 @@
 <?php
     include '../common/common.php';
-
+    $db = new Db("webproject",'');
+    
     $body=(array) json_decode(file_get_contents('php://input'));
-    $sender = $body["sender"];
-    $receiver = $body["receiver"];
+    $senderName = $body["sender"];
+    $receiverName = $body["receiver"];
     $msgType = $body["msgType"];
     $title = $body["title"];
     $content = $body["content"];
 
-    
+    if(is_numeric($receiverName))
+    {
+        $result=$db->select("SELECT username FROM users where number_theme = ?",[$receiverName]);
+        $receiverName=$result[0]["username"];
+    }
 
+    
     echo '{"status":"ok"}';
     //echo '{"status":"error"}';
-
 
 ?>
