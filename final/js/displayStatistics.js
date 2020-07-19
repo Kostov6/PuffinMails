74619093f5d47ff70ba27499f8f35c041e56e374
Serafim Babulkov <sefko989@gmail.com>
2020-07-20 00:29:47 +0300

    Add files via upload

--- final/js/displayStatistics.js
+++ final/js/displayStatistics.js
@@ -5,4 +5,9 @@ window.onload = function () {
     document.getElementById('rec_msg_count').textContent = statistics['recMsgCount'];
     document.getElementById('group_msg_count').textContent = statistics['groupMsgCount'];
     document.getElementById('seen_percentage').textContent = statistics['seenPercentage'];
+    document.getElementById('most_msg_group').textContent = statistics['groupMostMsgs'];
+    document.getElementById('most_msg_for_group').textContent = statistics['mostGroupMsgs'];
+    document.getElementById('most_rec_theme').textContent = statistics['mostRecTheme'];
+    document.getElementById('most_rec_for_theme').textContent = statistics['mostRecOnTheme'];
+    document.getElementById('user_most_msg').textContent = statistics['userMostMsgSent'];
 };
