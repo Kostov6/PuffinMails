570cedd0b0f2a3240e525c15572eca3869906f05
Serafim Babulkov <sefko989@gmail.com>
2020-07-21 14:04:51 +0300

    Add files via upload

--- final/php/reportsLoad.php
+++ final/php/reportsLoad.php
@@ -6,7 +6,7 @@
 
     $userId = $_SESSION['userID'];
 
-    $sql = "SELECT message.*, first_name, last_name FROM message INNER JOIN users ON message.senderId = users.userID WHERE msgType = 7";
+    $sql = "SELECT message.*, first_name, last_name FROM message INNER JOIN users ON message.senderId = users.userID WHERE msgType = 7 ORDER BY message.time_sent DESC";
     $reports = $db->select($sql,[]);
 
     $sql = "SELECT userID, first_name, last_name, faculty_number, ban_until FROM users WHERE ban_until > CURRENT_DATE()";
